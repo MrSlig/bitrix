847c8f30764fd30426cca5c80feb6ea8c699231f
SkillUP Student <student@skillup.spb.ru>
2018-01-17 21:23:25 +0300

    initial commit

---
+++ .top.menu.php
@@ -0,0 +1,53 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Компания", 
+		"company/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Новости", 
+		"news/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Продукция", 
+		"products/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Услуги", 
+		"services/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"contacts/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"about", 
+		"/login/about/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Skillup", 
+		"/skillup/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
